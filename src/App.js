d7b5630f29d2388b282345aea5a49c0de81f4239
TrevorTaylor-TP <ttaylor@talentpath.com>
2019-10-24 15:44:53 -0500

    added list items and functions for each

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ function App() {
 		<div className="App">
 			<header className="App-header">Zoomie Stuff</header>
 			<ZoomiePartzList>
+				<b>Zoomie Partz</b>
 				<ZoomiePartz>Blinky lights</ZoomiePartz>
 				<ZoomiePartz>Bumpy Bumper</ZoomiePartz>
+				<ZoomiePartz>Water Swishers</ZoomiePartz>
 			</ZoomiePartzList>
 		</div>
 	);
+}
+
+function ZoomiePartzList(props) {
+	return <ul>{props.children}</ul>;
+}
+
+function ZoomiePartz(props) {
+	return <li>{props.children}</li>;
 }
 
 export default App;
